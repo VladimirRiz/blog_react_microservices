b7629553a13b64a84abfad7b90566b684693c3ca
Vladimir Riz <resin.fhp@gmail.com>
2021-07-13 17:13:50 +0300

    feat: post routers

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const { randomBytes } = require('crypto');
 
 const app = express();
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/posts', (req, res) => {});
+app.use(express.json());
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  post[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log('listening on 4000');
