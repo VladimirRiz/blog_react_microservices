6ebdbb8622ccc3e98552ac7eb64e59682e1a129d
Vladimir Riz <resin.fhp@gmail.com>
2021-07-15 13:31:12 +0300

    feat: comments routers

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+app.use(express.json());
+
+const commentsById = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  res.send(commentsById[id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const { id } = req.params;
+  const comments = commentsById[id] || [];
+  comments.push({
+    id: commentId,
+    content,
+  });
+  commentsById[id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
