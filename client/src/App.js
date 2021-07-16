700e955190fe363b67c4aa623996d6e0e7dc29c6
Vladimir Riz <resin.fhp@gmail.com>
2021-07-16 14:44:59 +0300

    feat: PostList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PostCreate from './components/PostCreate';
+import PostList from './components/PostList';
+import axios from 'axios';
 
-const App = () => (
-  <div className="container">
-    <h1>Create Post</h1>
-    <PostCreate />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h2>Posts:</h2>
+      <PostList />
+    </div>
+  );
+};
 
 export default App;
