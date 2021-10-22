ba26b04947cf93a601d747f4c31d7ec728edcae0
Vladimir Riz <resin.fhp@gmail.com>
2021-10-22 16:49:41 +0300

    feat: use the query service

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
-  const [title, setTitle] = useState('');
+	const [title, setTitle] = useState("");
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post('http://localhost:4000/posts', { title });
-    setTitle('');
-  };
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await axios.post("http://localhost:4000/posts", { title });
+		setTitle("");
+	};
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-            className="form-control"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<div className="form-group">
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={({ target }) => setTitle(target.value)}
+						className="form-control"
+					/>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default PostCreate;
