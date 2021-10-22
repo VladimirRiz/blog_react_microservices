b330f867ad63daf6f4d1bc899b89aaa1afa73dea
Vladimir Riz <resin.fhp@gmail.com>
2021-10-22 13:56:39 +0300

    feat: add comment list

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,25 +9,26 @@ app.use(cors());
 
 const commentsById = {};
 
-app.get('/posts/:id/comments', (req, res) => {
-  const { id } = req.params;
-  res.send(commentsById[id] || []);
+app.get("/posts/:id/comments", (req, res) => {
+	const { id } = req.params;
+	console.debug(commentsById[id]);
+	res.status(201).send(commentsById[id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
-  const { content } = req.body;
-  const { id } = req.params;
-  const comments = commentsById[id] || [];
-  comments.push({
-    id: commentId,
-    content,
-  });
-  commentsById[id] = comments;
+app.post("/posts/:id/comments", (req, res) => {
+	const commentId = randomBytes(4).toString("hex");
+	const { content } = req.body;
+	const { id } = req.params;
+	const comments = commentsById[id] || [];
+	comments.push({
+		id: commentId,
+		content,
+	});
+	commentsById[id] = comments;
 
-  res.status(201).send(comments);
+	res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-  console.log('Listening on 4001');
+	console.log("Listening on 4001");
 });
